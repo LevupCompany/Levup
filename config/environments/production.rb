bdcb60983f42f5aafda78d6b755904b136fce2f2
LevupCompany <levup.company@gmail.com>
2018-03-19 18:59:54 +0200

    static_final

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,10 +25,10 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
@@ -85,7 +85,7 @@ Rails.application.configure do
     logger.formatter = config.log_formatter
     config.logger    = ActiveSupport::TaggedLogging.new(logger)
   end
-
+  config.secret_key_base = '<%= ENV["SECRET_KEY_BASE"] %>'
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 end
